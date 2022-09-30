f9c5a8ca7dea6836014c46db7b898b52a2d12d2c
Your Name <mareliza16@hotmail.com>
2022-09-29 20:23:36 -0500

    se crea el componente Botón y se hace el diseño de este

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import "./App.css";
-import freeCodeCampLogo from "./imagenes/FreeCodeCamp.png";
+import Boton from "./componentes/Boton";
+import freeCodeCampLogo from "./imagenes/freecodecamp.png";
 function App() {
+  const manejarClic = () => {
+    console.log("Clic");
+  };
+
+  const reiniciarContador = () => {
+    console.log("Reiniciar");
+  };
   return (
     <div className="App">
       <div className="freecodecamp-logo-contenedor"></div>
@@ -8,6 +16,12 @@ function App() {
         className="freecodecamp-logo"
         src={freeCodeCampLogo}
         alt="logo de freecodecamp"
+      />
+      <Boton texto="Clic" esBotonDeClic={true} manejarClic={manejarClic} />
+      <Boton
+        texto="Reiniciar"
+        esBotonDeClic={false}
+        manejarClic={reiniciarContador}
       />
     </div>
   );
